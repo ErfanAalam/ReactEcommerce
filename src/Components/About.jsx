2a8d884f7867e2f9c0043ed72ae99bd613d691c5
ErfanAalam <erfankhansiwani@gmail.com>
2024-11-10 16:40:36 +0530

    social media added

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,27 +1,51 @@
-import React from 'react'
-import { useState, useEffect } from 'react';
+// import React from "react";
+
+import InstagramIcon from "@mui/icons-material/Instagram";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import EmailIcon from "@mui/icons-material/Email";
 
 const About = () => {
+  return (
+    <>
+      <div>
+        <h3 className="about">
+          ClothHub: Your Ultimate Destination for Trendy and Affordable Fashion
+        </h3>
+        <p className="para">
+          Welcome to ClothHub, where style meets affordability! Discover an
+          extensive collection of clothing for every occasion—whether you’re
+          looking for casual wear, work essentials, or statement pieces for a
+          night out, ClothHub has you covered. Our curated selection combines
+          quality and the latest trends, making it easy to find styles that fit
+          your personality and budget. With user-friendly browsing, detailed
+          sizing guides, and a seamless checkout experience, shopping for your
+          favorite looks has never been easier. Join ClothHub and elevate your
+          wardrobe with pieces that make you look and feel great!
+        </p>
+      </div>
 
-  const [data , setData] = useState("")
-
-    const paragraphs = 20;
-    useEffect(() => {
-        fetch("https://api.api-ninjas.com/v1/loremipsum?paragraphs=" + paragraphs, {
-          headers: { "X-Api-Key": "w12daq2QFBqimbES0nRBlA==QkWtM2rJZtUUSoOI" },
-          contentType: "application/json",
-        })
-          .then((res) => res.json())
-          .then((result) => setData(result.text))
-          .catch((err) => console.log(err));
-      }, []);
-      return(<>
-        <h1>About</h1>
-        <div className="abouts">{data.split("\n").map((para,index)=>{
-            return <p key ={index}>{para}</p>
-        })}</div>
-        
-      </>)
-
-      }
-export default About
+      <div className="social">
+        <a
+          href="https://www.instagram.com/clothhub03/"
+          className="anchor">
+          <h2>Instagram</h2>
+          <InstagramIcon fontSize="large" />
+        </a>
+        <a
+          href="https://www.facebook.com/profile.php?id=61568254901212&mibextid=LQQJ4d"
+          className="anchor">
+          <h2>FaceBook</h2>
+          <FacebookIcon fontSize="large" />
+        </a>
+        <a
+          href="mailto:clothinghub67@gmail.com"
+          target="_blank"
+          className="anchor">
+          <h2>Email</h2>
+          <EmailIcon fontSize="large" />
+        </a>
+      </div>
+    </>
+  );
+};
+export default About;
